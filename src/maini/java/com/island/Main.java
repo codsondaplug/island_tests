e9ac2fe88e79efdcc64bf2f9d08e666eba265dde
codsondaplug <avistixbs@gmail.com>
2026-02-26 13:37:17 +0300

    Create Main.java

---
+++ src/maini/java/com/island/Main.java
@@ -0,0 +1,19 @@
+package com.island;
+
+import com.island.island.Island;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Симуляция запущена!");
+
+        Island island = new Island(5, 5);
+        island.startSimulation();
+
+        try {
+            Thread.sleep(60000);
+        } catch (InterruptedException e) {}
+
+        island.stopSimulation();
+        System.out.println("Симуляция завершена.");
+    }
+}
